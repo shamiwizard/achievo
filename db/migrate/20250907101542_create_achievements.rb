ab1b64a5386bde10fe034fc47063a5cd0c591d0f
Mykhailo Leibiuk <leibyuk04mi@gmail.com>
2025-09-07 13:17:29 +0300

    Add Achievement model

---
+++ db/migrate/20250907101542_create_achievements.rb
@@ -0,0 +1,12 @@
+class CreateAchievements < ActiveRecord::Migration[8.0]
+  def change
+    create_table :achievements do |t|
+      t.string :name, null:false
+      t.text :description, null: false
+      t.string :icon, null: false
+      t.integer :kind, null: false
+
+      t.timestamps
+    end
+  end
+end
