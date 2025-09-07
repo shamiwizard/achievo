ad8f6ca2b27bd2283e3ef64a0b96534e3685fc48
Mykhailo Leibiuk <leibyuk04mi@gmail.com>
2025-09-08 00:04:55 +0300

    Update rubocop, fix rubocop issue

--- db/migrate/20250907101542_create_achievements.rb
+++ db/migrate/20250907101542_create_achievements.rb
@@ -1,7 +1,7 @@
 class CreateAchievements < ActiveRecord::Migration[8.0]
   def change
     create_table :achievements do |t|
-      t.string :name, null:false
+      t.string :name, null: false
       t.text :description, null: false
       t.string :icon, null: false
       t.integer :kind, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,11 +16,11 @@ password = '12345Test'
 [
   {
     email: 'test@mail.com',
-    username: 'username1',
+    username: 'username1'
   },
   {
     email: 'test2@mail.com',
-    username: 'username2',
+    username: 'username2'
   }
 ].each do |user_data|
   User.find_or_create_by!(user_data) do |user|
