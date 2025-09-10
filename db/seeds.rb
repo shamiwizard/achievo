b1766d24a4c37ab56771e6e9c19a5c35bb9d6d85
Mykhailo Leibiuk <leibyuk04mi@gmail.com>
2025-09-10 21:54:43 +0300

    Fix rubocop issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@
 
 require 'faker'
 
-Dir[File.join(Rails.root, 'db', 'seeds', '*.rb' )].sort.each do |seed|
+Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
   load seed
 end
