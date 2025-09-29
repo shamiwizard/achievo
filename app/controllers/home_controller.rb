1e2d12552e8b8c883fea5ae874d75cc5cd14811c
Mykhailo Leibiuk <leibyuk04mi@gmail.com>
2025-09-29 22:19:26 +0300

    Split styles, update filter status styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @achievement_progresses = current_user.achievement_progresses.includes(achievement: :categories)
     @categories = Category.find(@achievement_progresses.distinct.pluck(:category_id))
+    @statuses = @achievement_progresses.distinct.pluck(:status)
   end
 end
